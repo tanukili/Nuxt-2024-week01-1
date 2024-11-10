27bb737f95c692570426c8d3b5bf13b714415c86
tanukile <backpacker320@gmail.com>
2024-11-10 20:39:37 +0800

    feat: 網站架構

---
+++ pages/admin/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Page: 後台 / 首頁</h1>
+    <p>目前路由的路徑 - route.fullPath 為：{{ $route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/product/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Page: 產品動態 id （product/[id]）</h1>
+    <p>目前路由的路徑 - route.fullPath 為：{{ $route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
