4de001dae6332af81ccc015f93705b9940e63e03
tanukile <backpacker320@gmail.com>
2024-11-10 20:38:53 +0800

    chore: 環境調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  // compatibilityDate 屬性 : 將 Nuxt3 的功能和行為鎖定在 2024-04-03 之前的版本，
+  // 避免之後 Nuxt3 新版本的寫法調整會影響到目前專案的運作
+  compatibilityDate: "2024-04-03",
+
+  // 啟用 Nuxt DevTools 開發工具
+  devtools: { enabled: true },
+
+  // 啟用 TypeScript
+  typescript: {
+    typeCheck: true,
+  },
+});
